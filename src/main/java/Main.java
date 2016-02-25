05d86605ccae735e43f1e06a74e83e657bdf96e0
SiyingZhang <siyingzhang@icloud.com>
2016-02-25 18:33:01 -0500

    modified the stylesheet type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -42,9 +42,10 @@ public class Main {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
-            return new ModelAndView(attributes, "index.html");
+            return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
-  */
+    */
+    
     get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
