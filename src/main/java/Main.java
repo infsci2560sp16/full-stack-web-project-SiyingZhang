2ba58dbbfed4c4a81453cd18dcf728294c714434
SiyingZhang <siyingzhang@icloud.com>
2016-03-29 01:48:02 -0400

    to see nav

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,13 +38,24 @@ public class Main {
       return "E=mc^2: " + energy + " = " + m.toString();
     });
 
-    /*get("/", (request, response) -> {
+    /*
+   get("/login", (req, res) -> {
+	   
+   }, new FreeMarkerEngine());*/
+    
+    get("/hello", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
-    */
+    
+    
+    get("/login.html", (req, res) -> {
+    	Map<String, Object> attributes = new HashMap<>();
+    	
+    	return new ModelAndView(attributes, "index.ftl");
+    }, new FreeMarkerEngine());
     
     get("/db", (req, res) -> {
       Connection connection = null;
